a3bbc9b279700eb210b88e25f6b5f115d3a005fc
Abraham Close <abraham@abrahamclose.com>
2019-12-11 17:46:44 -0500

    Removed citySelector, Scrapper, started new interface

--- public/render.js
+++ public/render.js
@@ -1,45 +1,32 @@
 export const makePage = function() {
     return `
-    <div class="main-page">
-        <h1 class="logo">SportsCity</h1>
-        <h1 class="city-choice">City:</h1>
-        <h2><a href="citySelector.html">Reset City</a></h2>
-        <h1 class="nba">Latest NBA Score:</h1>
-        <h1 class="nfl">Latest NFL Score:</h1>
-        <h1 class="nhl">Latest NHL Score:</h1>
-        <h1 class="mlb">Latest MLB Score:</h1>
-    </div>`;
-};
+        <div class="tile is-ancestor">
+            <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Hello World</p>
+                <p class="subtitle">What is up?</p>
+                <div class="content">
 
-export const makePageWithEnterCity = function() {
-    return `
-    <div class="main-page">
-        <button class="reset-city" type="button">Reset City</button>
-        <h1 class="logo">SportsCity</h1>
-        <h1 class="enter-city">Enter City:</h1>
-        <textarea class="input" id="cityTextArea"></textarea>
-        <br/>
-        <button class="submit-new-city" type="button">Submit</button>
-        <h1 class="nba">Latest NBA Score:</h1>
-        <h1 class="nfl">Latest NFL Score:</h1>
-        <h1 class="nhl">Latest NHL Score:</h1>
-        <h1 class="mlb">Latest MLB Score:</h1>
-    </div>`;
+                </div>
+            </article>
+            </div>
+            <div class="tile is-parent">
+            <article class="tile is-child box">
+                <p class="title">Foo</p>
+                <p class="subtitle">Bar</p>
+                <div class="content">
+
+                </div>
+            </article>
+            </div>
+        </div>
+        `;
 };
 
 export const loadSportsPage = function() {
     const $root = $('.root');
 
-    //Later, we can add a parameter to pass into our makePage function to load the correct scores
-    $root.append(makePage());
-    $root.on("click", '.reset-city', function(){
-        $root.empty();
-        $root.append(makePageWithEnterCity);
-    });
-    $root.on("click", '.submit-new-city', function(){
-        $root.empty();
-        $root.append(makePage);
-    });
+    $root.append(makePage2());
 }
 
 $(function() {
